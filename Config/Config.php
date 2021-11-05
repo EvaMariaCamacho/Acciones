a87ecdd51f17f4f2c9afb461e8bb6627a05b18d9
Eva Maria <evamariacamacho@gmail.com>
2021-11-05 14:40:23 +0100

    commint carpetas bases

--- Config/Config.php
+++ Config/Config.php
@@ -1,17 +1,16 @@
 <?php 
 	
-	//define("BASE_URL", "http://localhost/accionesf/");
-	const BASE_URL = "http://localhost/acciones";
+	const BASE_URL = "https://localhost/acciones";
 
 	//Zona horaria
 	date_default_timezone_set('Europe/Madrid');
 
 	//Datos de conexión a Base de Datos
 	const DB_HOST = "localhost";
-	const DB_NAME = "db_acciones";
+	const DB_NAME = "tienda";
 	const DB_USER = "root";
 	const DB_PASSWORD = "root";
-	const DB_CHARSET = "utf8";
+	const DB_CHARSET = "charset=utf8";
 
 	//Deliminadores decimal y millar Ej. 24,1989.00
 	const SPD = ".";
@@ -19,6 +18,7 @@
 
 	//Simbolo de moneda
 	const SMONEY = "€";
+
 	
 
 
