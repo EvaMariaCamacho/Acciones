1782d5b6bdf27f4903022dda72e609c1ba99121f
Eva Maria <evamariacamacho@gmail.com>
2021-11-05 10:06:49 +0100

    commint inicio

---
+++ Config/Config.php
@@ -0,0 +1,25 @@
+<?php 
+	
+	//define("BASE_URL", "http://localhost/accionesf/");
+	const BASE_URL = "http://localhost/acciones";
+
+	//Zona horaria
+	date_default_timezone_set('Europe/Madrid');
+
+	//Datos de conexión a Base de Datos
+	const DB_HOST = "localhost";
+	const DB_NAME = "db_acciones";
+	const DB_USER = "root";
+	const DB_PASSWORD = "root";
+	const DB_CHARSET = "utf8";
+
+	//Deliminadores decimal y millar Ej. 24,1989.00
+	const SPD = ".";
+	const SPM = ",";
+
+	//Simbolo de moneda
+	const SMONEY = "€";
+	
+
+
+ ?>
