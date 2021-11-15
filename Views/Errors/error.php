a5b915fd2f175390f945a32a09b1a028ac7310b1
Eva Maria <evamariacamacho@gmail.com>
2021-11-15 12:53:49 +0100

    commint formulario de contacto

--- Views/Errors/error.php
+++ Views/Errors/error.php
@@ -64,45 +64,10 @@
 
 
     <!-- Footer -->
-    <div class="footer bg-gray">
+    <div class="footer bg-gray m-5">
         <img class="decoration-city" src="<?= media(); ?>/images/decoration-city.svg" alt="alternative">
         <div class="container">
             <div class="row">
-                <div class="col-lg-12">
-                    <h4>Nuestras redes sociales</h4>
-                    <div class="social-container">
-                        <span class="fa-stack">
-                            <a href="#your-link">
-                                <i class="fas fa-circle fa-stack-2x"></i>
-                                <i class="fab fa-facebook-f fa-stack-1x"></i>
-                            </a>
-                        </span>
-                        <span class="fa-stack">
-                            <a href="#your-link">
-                                <i class="fas fa-circle fa-stack-2x"></i>
-                                <i class="fab fa-twitter fa-stack-1x"></i>
-                            </a>
-                        </span>
-                        <span class="fa-stack">
-                            <a href="#your-link">
-                                <i class="fas fa-circle fa-stack-2x"></i>
-                                <i class="fab fa-pinterest-p fa-stack-1x"></i>
-                            </a>
-                        </span>
-                        <span class="fa-stack">
-                            <a href="#your-link">
-                                <i class="fas fa-circle fa-stack-2x"></i>
-                                <i class="fab fa-instagram fa-stack-1x"></i>
-                            </a>
-                        </span>
-                        <span class="fa-stack">
-                            <a href="#your-link">
-                                <i class="fas fa-circle fa-stack-2x"></i>
-                                <i class="fab fa-youtube fa-stack-1x"></i>
-                            </a>
-                        </span>
-                    </div> <!-- end of social-container -->
-                </div> <!-- end of col -->
             </div> <!-- end of row -->
         </div> <!-- end of container -->
     </div> <!-- end of footer -->  
--- Views/home.php
+++ Views/home.php
@@ -51,6 +51,9 @@
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#header">Inicio</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="<?= base_url(); ?>/Views/Contact/contact.php">contacto</a>
+                    </li>
                 </ul>
                 <span class="nav-item">
                     <a class="btn-solid-sm" href="#contact">Login</a>
@@ -522,97 +525,13 @@
     </div> <!-- end of filter -->
     <!-- end of projects -->
 
-    <!-- Contact -->
-    <div id="contact" class="form-1">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h2 class="h2-heading"><span>Contacto</h2>
-                    <!-- <p class="p-heading">Vel malesuada sapien condimentum nec. Fusce interdum nec urna et finibus pulvinar tortor id</p> -->
-                    <ul class="list-unstyled li-space-lg">
-                        <li><i class="fas fa-map-marker-alt"></i> &nbsp;Dirección</li>
-                        <li><i class="fas fa-phone"></i> &nbsp;<a href="">Telefono</a></li>
-                        <li><i class="fas fa-envelope"></i> &nbsp;<a href="mailto:lorem@ipsum.com">lorem@ipsum.com</a></li>
-                    </ul>
-                </div> <!-- end of col -->
-            </div> <!-- end of row -->
-            <div class="row">
-                <div class="col-lg-10 offset-lg-1">
-                    
-                    <!-- Contact Form -->
-                    <form>
-                        <div class="form-group">
-                            <input type="text" class="form-control-input" placeholder="Name" required>
-                        </div>
-                        <div class="form-group">
-                            <input type="email" class="form-control-input" placeholder="Email" required>
-                        </div>
-                        <div class="form-group">
-                            <select class="form-control-select" required>
-                                <option class="select-option" value="" disabled selected>Project type</option>
-                                <option class="select-option" value="Company Website">Company Website</option>
-                                <option class="select-option" value="Landing Page">Landing Page</option>
-                                <option class="select-option" value="Online Shop">Online Shop</option>
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <textarea class="form-control-textarea" placeholder="Message" required></textarea>
-                        </div>
-                        <div class="form-group">
-                            <button type="submit" class="form-control-submit-button">Submit</button>
-                        </div>
-                    </form>
-                    <!-- end of contact form -->
-
-                </div> <!-- end of col -->
-            </div> <!-- end of row -->
-        </div> <!-- end of container -->
-    </div> <!-- end of form-1 -->
-    <!-- end of contact -->
+   
 
 
     <!-- Footer -->
-    <div class="footer bg-gray">
+    <div class="footer bg-gray m-5">
         <img class="decoration-city" src="Assets/images/decoration-city.svg" alt="alternative">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h4>Nuestras redes sociales</h4>
-                    <div class="social-container">
-                        <span class="fa-stack">
-                            <a href="#your-link">
-                                <i class="fas fa-circle fa-stack-2x"></i>
-                                <i class="fab fa-facebook-f fa-stack-1x"></i>
-                            </a>
-                        </span>
-                        <span class="fa-stack">
-                            <a href="#your-link">
-                                <i class="fas fa-circle fa-stack-2x"></i>
-                                <i class="fab fa-twitter fa-stack-1x"></i>
-                            </a>
-                        </span>
-                        <span class="fa-stack">
-                            <a href="#your-link">
-                                <i class="fas fa-circle fa-stack-2x"></i>
-                                <i class="fab fa-pinterest-p fa-stack-1x"></i>
-                            </a>
-                        </span>
-                        <span class="fa-stack">
-                            <a href="#your-link">
-                                <i class="fas fa-circle fa-stack-2x"></i>
-                                <i class="fab fa-instagram fa-stack-1x"></i>
-                            </a>
-                        </span>
-                        <span class="fa-stack">
-                            <a href="#your-link">
-                                <i class="fas fa-circle fa-stack-2x"></i>
-                                <i class="fab fa-youtube fa-stack-1x"></i>
-                            </a>
-                        </span>
-                    </div> <!-- end of social-container -->
-                </div> <!-- end of col -->
-            </div> <!-- end of row -->
-        </div> <!-- end of container -->
+       
     </div> <!-- end of footer -->  
     <!-- end of footer -->
 
