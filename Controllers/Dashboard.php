a351105187b289ecf305f146ee15b3b1dda42548
Eva Maria <evamariacamacho@gmail.com>
2021-11-15 15:53:48 +0100

    commit views de permisos y roles

--- Controllers/Dashboard.php
+++ Controllers/Dashboard.php
@@ -9,8 +9,8 @@
 		public function dashboard()
 		{
 			$data['page_id'] = 2;
-			$data['page_tag'] = "Dashboard - Tienda Virtual";
-			$data['page_title'] = "Dashboard - Tienda Virtual";
+			$data['page_tag'] = "Dashboard - Acciones";
+			$data['page_title'] = "Dashboard - Acciones";
 			$data['page_name'] = "dashboard";
 			$this->views->getView($this,"dashboard",$data);
 		}
